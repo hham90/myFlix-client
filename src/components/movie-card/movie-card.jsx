5e0c3288e7f59e241ed242c319e6fcdaed2f9448
hham90 <148152147+hham90@users.noreply.github.com>
2023-12-20 16:07:27 -0500

    updated movieard with bootstrap
    added Button
    added Card

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,22 +1,25 @@
-import PropTypes from "prop-types"
+import PropTypes from "prop-types";
+import { Button, Card } from "react-bootstrap";
 
 export const MovieCard = ({movie, onMovieClick}) => {
     return (
-        <div
-          onClick={() =>{
-            onMovieClick(movie);
-          }}
-    >
-        {movie.title}
-        </div>
+        <Card className="h-100" onClick={() => onMovieClick(movie)}>
+        <Card.Img variant="top" src={movie.img} />
+        <Card.Body>
+          <Card.Title>{movie.title}</Card.Title>
+          <Card.Text>{movie.director}</Card.Text>
+         <Button onClick={() => onMovieClick(movie)} variant="link">
+          OPEN
+         </Button>
+         </Card.Body>
+         </Card>
     );
 };
 
 MovieCard.propTypes = {
   movie: PropTypes.shape({
     title: PropTypes.string.isRequired,
-    director: PropTypes.string,
-    description: PropTypes.string.isRequired
+    director: PropTypes.string
   }).isRequired,
   onMovieClick: PropTypes.func.isRequired
 };
